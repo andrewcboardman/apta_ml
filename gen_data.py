a1b53d184d0631551451de185af6b13654588c00
Andrew <acb95@cam.ac.uk>
2019-02-01 13:04:03 +0000

    Improved ising model simulation

--- gen_data.py
+++ gen_data.py
@@ -3,14 +3,31 @@ from Bio.SeqRecord import SeqRecord
 from Bio.Seq import Seq
 import numpy as np
 
+def IsingEnergy(x,h,J,L):
+	"""Finds the Ising energy for a sample sequence"""
+	field_energy = -np.sum(h[range(L),x])
+	coupling_energy = -np.sum(J[range(L),x,range(L),x])
+	return field_energy + coupling_energy
+
+def EnergyChange(x,new_base,pos,J,h,L):
+	# Field energy for new base - field energy for new base
+	
+	dE = - h[pos,x.astype(int)[pos]] + h[pos,new_base]
+	# coupling energy for new base
+	dE = dE - np.sum(J[range(L),x,pos,x[pos]]) + np.sum(J[range(L),x,pos,new_base])
+	return dE
+
 def MonteCarloStep(x,L,J,h):
 	pos = np.random.randint(L)
 	new_base = np.random.randint(4)
-	dE = EnergyChange(x[pos],new_base,pos)
-	dx[np.random.random.randint(L),np.random.randint(3)] = 1
+	dE = EnergyChange(x,new_base,pos,J,h,L)
+	if np.exp(-dE) > np.random.rand():
+		x[pos] = new_base
+	return x
 
-def next_array(x,L,J,h,N):
-	for i in range(N):
+
+def next_array(x,L,J,h,n):
+	for i in range(n):
 		x = MonteCarloStep(x,L,J,h)
 	return x
 
@@ -21,10 +38,26 @@ def num2string(x,L):
 	seq[x==3] = 'T'
 	return ''.join(seq)
 
-N = 100000
-L = 40
+def main():
+	N = 100 # number of samples to generate
+	L = 40 # number of spins
+	q = 4 # number of states
+	n = 10 # number of Monte Carlo steps between samples
 
-nums = (np.random.randint(4,size=L) for x in range(N))
-strings = (num2string(num,L) for num in nums)
-seqs = (SeqRecord(Seq(string),id=f'sample {i}') for i,string in enumerate(strings))
-SeqIO.write(seqs,'random_seqs.fasta','fasta')
+	# Initialise simulation and save coefficients
+	h = np.random.rand(L,q)/np.sqrt(120)
+	np.savetxt('h.txt',h)
+	J = np.random.rand(L,q,L,q)/np.sqrt(120)
+	J = 0.5*(J+J.transpose(2,3,0,1)) # symmetrise J
+	nums = np.zeros((N,L)).astype(int)
+	energy = np.zeros(N)
+	nums[0,...] = np.random.randint(q,size=L)
+	for i in range(1,N):
+		nums[i,...] = next_array(nums[i-1,...],L,J,h,n)
+		energy[i] = IsingEnergy(nums[i,...],h,J,L)
+	strings = (num2string(num,L) for num in nums)
+	seqs = (SeqRecord(Seq(string),id=f'sample {i}') for i,string in enumerate(strings))
+	SeqIO.write(seqs,'test_seqs.fasta','fasta')
+
+if __name__ == '__main__':
+	main()
--- get_data.py
+++ get_data.py
@@ -2,19 +2,20 @@ import numpy as np
 
 
 # Load binding sequences
-x_bind = np.genfromtxt('data/plus_encode.txt')
+x_bind = np.genfromtxt('data/sk/sk_encode.txt')
 n_bind = x_bind.shape[0]
 
 # Load control sequences
-x_control = np.genfromtxt('data/minus_encode.txt')
+x_control = np.genfromtxt('data/sk/sk_encode.txt')
 n_control = x_control.shape[0]
 
 # Combine binding and control
 x = np.concatenate((x_bind,x_control))
 y = np.concatenate((np.ones(n_bind),np.zeros(n_control)))
 
-
-ids = np.genfromtxt('data/IgE_shuffle_ids.txt').astype(int)
+ids = np.array(range(y.size))
+np.random.shuffle(ids)
+np.savetxt('data/sk/sk_shuffle_ids.txt',ids.astype(int))
 x = x[ids,...]
 y = y[ids]
 
@@ -23,7 +24,8 @@ x_means = np.mean(x,axis=0)
 x_stds = np.std(x,axis=0)
 x_stand = (x - x_means) / x_stds
 
-train_test = np.genfromtxt('data/IgE_train_test.txt').astype(bool)
+train_test = np.random.rand(shape=y.size) > 0.8
+np.savetxt('data/sk/sk_train_test.txt',train_test.astype(bool))
 
 x_train = x[np.logical_not(train_test),...]
 x_train_stand = x_stand[np.logical_not(train_test),...]
@@ -32,10 +34,10 @@ x_test = x[train_test,...]
 x_test_stand = x_stand[train_test,...]
 y_test = y[train_test]
 
-np.save('data/IgE_train_features',x_train)
-np.save('data/IgE_train_features_stand',x_train_stand)
-np.save('data/IgE_train_labels',y_train)
-np.save('data/IgE_test_features',x_test)
-np.save('data/IgE_test_features_stand',x_test_stand)
-np.save('data/IgE_test_labels',y_test)
+np.save('data/sk/sk_train_features',x_train)
+np.save('data/sk/sk_train_features_stand',x_train_stand)
+np.save('data/sk/sk_train_labels',y_train)
+np.save('data/sk/sk_test_features',x_test)
+np.save('data/sk/sk_test_features_stand',x_test_stand)
+np.save('data/sk/sk_test_labels',y_test)
 
